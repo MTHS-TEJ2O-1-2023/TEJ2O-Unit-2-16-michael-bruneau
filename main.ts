8038d9adb612223c2b17971f233dc213520efc26
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-12-06 12:34:06 -0500

    sonar

--- main.ts
+++ main.ts
@@ -8,3 +8,11 @@
 // setup
 radio.setGroup(7)
 basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  sonar.ping(
+    DigitalPin.P1, 
+    DigitalPin.P2, 
+    PingUnit.Centimeters
+  )
+})
